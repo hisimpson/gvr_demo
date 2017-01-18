1dfac318ed9bc453838d7fdd077667c9d06ca81a
wow <wow@daum-naver.com>
2017-01-18 15:34:02 +0900

    VR 모드, NONE VR Mode 체인지 daydream, cardboard 모드 적용

--- Assets/GoogleVR/Scripts/Controller/Internal/Emulator/EmulatorConfig.cs
+++ Assets/GoogleVR/Scripts/Controller/Internal/Emulator/EmulatorConfig.cs
@@ -54,7 +54,7 @@ namespace Gvr.Internal {
 
     // IP address of the phone, when connected to the PC via WiFi.
     //public static readonly string WIFI_SERVER_IP = "192.168.43.1";
-    public static readonly string WIFI_SERVER_IP = "192.168.219.102";
+    public static readonly string WIFI_SERVER_IP = "192.168.219.100";
     }
 }
 /// @endcond
---
+++ Assets/script/vrmode/SwitchVRMode.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.VR;
+using UnityEngine.UI;
+
+public class SwitchVRMode : MonoBehaviour
+{
+    float nextTransition = 5.0f;
+
+    float transitionTime;
+    enum Mode { NOVR, VR, MAX };
+    Mode mode;
+
+    enum UseVrDevice { NONE, CARDBOARD, DAYDREAM };
+    UseVrDevice useVrDevice = UseVrDevice.NONE;
+        
+    // Use this for initialization
+    void Start()
+    {
+        mode = Mode.VR;
+
+        transitionTime = Time.timeSinceLevelLoad + nextTransition;
+        StartCoroutine(ChangeMode(mode));
+
+        string[] supportedDevices = VRSettings.supportedDevices;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    IEnumerator ChangeMode(Mode emode)
+    {
+        if (mode == Mode.NOVR)
+            VRSettings.LoadDeviceByName("none");
+        else if (mode == Mode.VR)
+        { 
+            //If failed loading daydream device, load "cardboard" device.
+            VRSettings.LoadDeviceByName("daydream");
+            //VRSettings.LoadDeviceByName("cardboard");
+        }
+
+        yield return null;
+
+        if (mode == Mode.VR)
+        {
+            VRSettings.enabled = true;
+            if (VRDevice.model.IndexOf("daydream", StringComparison.CurrentCultureIgnoreCase) > -1)
+                useVrDevice = UseVrDevice.DAYDREAM;
+            else
+                useVrDevice = UseVrDevice.CARDBOARD;
+        }
+
+
+#if UNITY_EDITOR
+        if (mode == Mode.VR)
+            GvrViewer.Instance.VRModeEnabled = true;
+        else
+            GvrViewer.Instance.VRModeEnabled = false;
+#endif
+    }
+}
